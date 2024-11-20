01e6d385777465a9cde17ac8c8eb7464d4bad540
bmbx08 <bmbx08@bu.ac.kr>
2024-11-21 02:42:35 +0900

    separated from login-signup

--- src/pages/LoginSignupPage/SignupPage/SignupPage.jsx
+++ src/pages/LoginSignupPage/SignupPage/SignupPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Form from "react-bootstrap/Form";
-import '../LoginSignupPage/SignupPage.css';
+import './SignupPage.css';
 
 const SignupPage = () => {
   return (
