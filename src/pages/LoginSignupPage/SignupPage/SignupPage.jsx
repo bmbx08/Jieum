b575bc9b4052ea9d45b9a7886ca9dce49f50e3ef
bmbx08 <bmbx08@bu.ac.kr>
2024-11-21 03:36:24 +0900

    test git push again

--- src/pages/LoginSignupPage/SignupPage/SignupPage.jsx
+++ src/pages/LoginSignupPage/SignupPage/SignupPage.jsx
@@ -6,6 +6,7 @@ import './SignupPage.css';
 const SignupPage = () => {
   const [page, setPage]= useState(1);
 
+  
   return (
     <div className="display-center">
       <h2 className="register-header">백석지음[知音] 회원가입</h2>
