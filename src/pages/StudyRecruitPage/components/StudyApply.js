b3403a6a49a38781fea59cc66f8b0c0cce648c16
Khaneul1 <kosky1219@gmail.com>
2024-11-21 14:45:51 +0900

    Add StudyRecruitPage to check-post branch

---
+++ src/pages/StudyRecruitPage/components/StudyApply.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import Swal from 'sweetalert2';
+import './StudyApply.css';
+
+const StudyApply = ({ onApplicantAdd, userName }) => {
+  const [applicants, setApplicants] = useState([]); //신청자 목록 상태 관리
+
+  const handleApply = () => {
+    const newApplicant = {
+      name: { userName },
+      timestamp: new Date().toLocaleString(),
+    };
+
+    setApplicants((prevApplicants) => {
+      const updatedApplicants = [...prevApplicants, newApplicant];
+      onApplicantAdd(updatedApplicants);
+      return updatedApplicants;
+    });
+
+    Swal.fire({
+      position: 'top-end',
+      icon: 'success',
+      title: '신청되었습니다',
+      showConfirmButton: false,
+      timer: 1500,
+    });
+
+    console.log('현재 신청자 목록:', [...applicants, newApplicant]);
+  };
+
+  return (
+    <div className="button-box">
+      <button className="apply-button" onClick={handleApply}>
+        신청하기
+      </button>
+    </div>
+  );
+};
+
+export default StudyApply;
