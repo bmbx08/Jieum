ac20ecdea011e9351ba44ad4389e39a67934c832
bmbx08 <bmbx08@bu.ac.kr>
2024-11-26 03:27:46 +0900

    moved SubNav to AllStudiesPage folder

--- src/pages/AllStudiesPage/AllStudiesPage.jsx
+++ src/pages/AllStudiesPage/AllStudiesPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import SideBar from '../../default/SideBar';
-import SubNav from '../../default/SubNav';
+import SubNav from './components/SubNav';
 import SearchBar from './components/SearchBar';
 import StudyList from './components/StudyList';
 import StudyStatus from './components/StudyStatus';
