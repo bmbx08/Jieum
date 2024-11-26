5b226c327b1e3c85369f3b0708f4cc3069a21716
bmbx08 <bmbx08@bu.ac.kr>
2024-11-26 20:45:47 +0900

    Revert "Merge pull request #36 from bmbx08/signup-page"
    This reverts commit 6af5d20876701b5bf1734945d4cc62daa462a17b, reversing
    changes made to 5c7e5ede014d8ed5e7033b24dd5dedbb7bf9c834.

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from 'react';
+import './Homepage.style.css';
 
 const Homepage = () => {
   return (
     <div>
       Homepage
     </div>
-  )
-}
+  );
+};
 
-export default Homepage
+export default Homepage;
